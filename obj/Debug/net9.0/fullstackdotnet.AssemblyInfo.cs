7a44575f084044966d8ebdc365e6c8ba015a8c99
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-01-15 09:28:18 +0500

    new files

--- obj/Debug/net9.0/fullstackdotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/fullstackdotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fullstackdotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d235d93c917c3033c5b3d69306d82affc5c58b33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717fe3959ab2eb4a33b3fdc0f8e629c218bb7346")]
 [assembly: System.Reflection.AssemblyProductAttribute("fullstackdotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fullstackdotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
